17dfa1804ad445120c566082d2fb3f6eee37fd7f
Timur Timaev <chemradio@gmail.com>
2023-12-14 12:36:17 +0600

    iter

--- helpers/db_parser.py
+++ helpers/db_parser.py
@@ -5,20 +5,54 @@ from pathlib import Path
 
 def parse_database(db_path: Path | str) -> list[dict]:
     with open(db_path, "rt") as f:
-        raw_data: list[dict] = json.load(f)
+        database: list[dict] = json.load(f)
 
-    for program in raw_data:
+    for program in database:
         if not program["start"]:
-            raw_data.remove(program)
+            raise ValueError(f"Program {program.get('name')} has no start time.")
+            # raw_data.remove(program)
 
         program_start = datetime.time.fromisoformat(program["start"])
-        print(program_start)
+        program["start"] = program_start
 
         if not program["duration"] and not program["end"]:
-            raw_data.remove(program)
+            raise ValueError(
+                f"Can't determine end time for program {program.get('name')}."
+            )
+            # raw_data.remove(program)
 
-        if program["end"] and program["duration"]:
-            program.pop("duration")
-        else:
+        if program["duration"]:
+            datetime.datetime.combine(
+                datetime.datetime(year=2023, month=1, day=1), program_start
+            )
+
             duration = program.pop("duration")
-            program["end"]
+            duration_hours, duration_minutes, duration_seconds = parse_iso(duration)
+            program_end = datetime.time(
+                hour=program_start.hour + duration_hours,
+                minute=program_start.minute + duration_minutes,
+                second=program_start.second + duration_seconds,
+            )
+
+        elif program["end"]:
+            program_end = datetime.time.fromisoformat(program["end"])
+            if program_end <= program_start:
+                raise ValueError(
+                    f"Program {program.get('name')} end time is earlier than it's start."
+                )
+        program["end"] = program_end
+
+    return database
+
+
+def parse_iso(iso_time: str) -> tuple[int]:
+    hours, minutes, seconds = 0, 0, 0
+    time_array = iso_time.split(":")
+    if len(time_array) < 2:
+        raise ValueError("Wrong iso timestring. Less than two colon-separated values")
+    minutes = int(time_array[-2])
+    seconds = int(time_array[-1])
+    if len(time_array) > 2:
+        hours = int(time_array[-3])
+
+    return hours, minutes, seconds
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import datetime
+from pprint import pprint
 
 from helpers.db_parser import parse_database
 
-db = parse_database("./program_starts.json")
+print(dummy_datetime)
+# db = parse_database("./program_starts.json")
+# pprint(db)
 
-current_datetime = datetime.datetime.utcnow() + datetime.timedelta(hours=6)
+# current_datetime = datetime.datetime.utcnow() + datetime.timedelta(hours=6)
 
-# for program in db:
-#     if current_datetime.weekday not in program["weekdays"]:
-#         continue
+# # for program in db:
+# #     if current_datetime.weekday not in program["weekdays"]:
+# #         continue
 
-x = current_datetime.time()
-y = datetime.time(hour=23, minute=59)
-print(x > y)
+# x = current_datetime.time()
+# y = datetime.time(hour=23, minute=59)
+# print(x > y)
