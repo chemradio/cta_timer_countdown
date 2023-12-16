6f518ba3b9b13522c8e35dc182ac151e4cbc1a7a
Timur Timaev <chemradio@gmail.com>
2023-12-16 22:42:39 +0600

    .

--- helpers/time_formatter.py
+++ helpers/time_formatter.py
@@ -3,6 +3,9 @@ import datetime
 from enums.digit_display import DigitDisplay
 from helpers.components import get_time_components_from_delta
 
+SIX_DIGIT_IDLE_FORMATS = ["IDLE. . .", "IDLE  ", "IDLE.  ", "IDLE. . "]
+SIX_DIGIT_IDLE_PHASE = 0
+
 
 def format_time(delta: datetime.timedelta, display_type: DigitDisplay):
     if display_type == DigitDisplay.EIGHT_DOTTED:
@@ -17,7 +20,11 @@ def format_time_eight_digit_dotted(delta: datetime.timedelta) -> str:
 
 def format_time_six_digit_dotted(delta: datetime.timedelta) -> str:
     if delta is None:
-        return "IDLE. . ."
+        if SIX_DIGIT_IDLE_PHASE < len(SIX_DIGIT_IDLE_FORMATS):
+            return SIX_DIGIT_IDLE_FORMATS[SIX_DIGIT_IDLE_PHASE]
+        else:
+            SIX_DIGIT_IDLE_PHASE = 0
+            return SIX_DIGIT_IDLE_FORMATS[SIX_DIGIT_IDLE_PHASE]
 
     hours, minutes, seconds, microseconds = get_time_components_from_delta(delta)
     return "{:02d}.{:02d}.{:02d}".format(hours, minutes, seconds)
