0fab93c647e62a18b1f50929dc30f4165607e4d0
Timur Timaev <chemradio@gmail.com>
2023-12-17 14:26:45 +0600

    .

--- helpers/track_time.py
+++ helpers/track_time.py
@@ -12,25 +12,14 @@ def track_time(
     if not db:
         return TimerStatus.IDLE, None
 
-    # weekdays_with_programs = list()
+    current_time = current_datetime.time()
+
     for program in db:
         if current_datetime.weekday() not in program["weekdays"]:
             continue
 
-        current_time = current_datetime.time()
-
         if current_time >= program["end"]:
-            today_weekday = current_datetime.weekday()
-            tomorrow_weekday = 0 if today_weekday == 6 else today_weekday + 1
-            if tomorrow_weekday in program["weekdays"]:
-                tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-                tomorrow_start_datetime = datetime.datetime.combine(
-                    tomorrow, program["start"], get_target_tz()
-                )
-                diff = tomorrow_start_datetime - current_datetime
-                return TimerStatus.COUNTDOWN_TOMORROW, diff
-            else:
-                continue
+            continue
 
         if current_time < program["start"]:
             program_start_datetime = datetime.datetime.combine(
@@ -46,4 +35,16 @@ def track_time(
             diff = program_end_datetime - current_datetime
             return TimerStatus.ONAIR, diff
 
-    return TimerStatus.IDLE, None
+    else:
+        for program in db:
+            today_weekday = current_datetime.weekday()
+            tomorrow_weekday = 0 if today_weekday == 6 else today_weekday + 1
+            if tomorrow_weekday in program["weekdays"]:
+                tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+                tomorrow_start_datetime = datetime.datetime.combine(
+                    tomorrow, program["start"], get_target_tz()
+                )
+                diff = tomorrow_start_datetime - current_datetime
+                return TimerStatus.COUNTDOWN_TOMORROW, diff
+
+        return TimerStatus.IDLE, None
